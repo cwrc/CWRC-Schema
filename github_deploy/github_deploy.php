9de060b8fff69ceee3043d300c551741a9d775b2
Jeffery Antoniuk <jefferya@ualberta.ca>
2014-06-12 16:21:38 -0600

    fix bug deploy

--- github_deploy/github_deploy.php
+++ github_deploy/github_deploy.php
@@ -15,7 +15,7 @@ if (file_exists($LOCAL_REPO)) {
 }
 else {
   // if repo doesn't exist then clone and checkout branch 
-  echo shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO} {$LOCAL_REPO_NAME} && cd {$LOCAL_REPO} && git checkout {$BRANCH}");
+  echo shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO} {$LOCAL_REPO_NAME} && cd {$LOCAL_REPO} && git checkout {$DESIRED_BRANCH}");
 }
 
 
@@ -27,6 +27,7 @@ else {
 // Clone fresh repo from github using desired local repo name and checkout the desired branch
 //echo shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO} {$LOCAL_REPO_NAME} && cd {$LOCAL_REPO} && git checkout {$BRANCH}");
 
-die("done " . mktime());
+date_default_timezone_set('America/Edmonton');
+die("done " . date('Y-m-d h:i:s a', time()) . "\n"  ) ;
 
 ?>
