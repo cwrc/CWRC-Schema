f13dd360459ad821a702b1b9a904bc0b2b920376
Jeffery Antoniuk <jefferya@ualberta.ca>
2014-06-12 15:55:01 -0600

    added Github WebHooks deployment

---
+++ github_deploy/github_deploy.php
@@ -0,0 +1,32 @@
+<?php
+
+// https://jonathanstark.com/blog/deploying-code-automatically-with-github-webhooks
+
+// Init vars
+$LOCAL_ROOT         = "/data/opt/cwrc";
+$LOCAL_REPO_NAME    = "CWRC-Schema";
+$LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
+$REMOTE_REPO        = "https://github.com/cwrc/CWRC-Schema.git";
+$DESIRED_BRANCH     = "master";
+
+if (file_exists($LOCAL_REPO)) {
+  // if repo exists then pull down updated
+  echo shell_exec("cd {$LOCAL_REPO} && git pull");
+}
+else {
+  // if repo doesn't exist then clone and checkout branch 
+  echo shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO} {$LOCAL_REPO_NAME} && cd {$LOCAL_REPO} && git checkout {$BRANCH}");
+}
+
+
+// Delete local repo if it exists
+//if (file_exists($LOCAL_REPO)) {
+    //shell_exec("rm -rf {$LOCAL_REPO}");
+//}
+
+// Clone fresh repo from github using desired local repo name and checkout the desired branch
+//echo shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO} {$LOCAL_REPO_NAME} && cd {$LOCAL_REPO} && git checkout {$BRANCH}");
+
+die("done " . mktime());
+
+?>
